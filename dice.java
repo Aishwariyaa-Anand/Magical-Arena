7d3de5a489509e2d3b12efdc90637e90971008b4
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-07-10 11:03:08 +0530

    roll dice class

--- dice.java
+++ dice.java
@@ -1,11 +1,14 @@
+import java.util.Random;
+
 public class Dice {
+    private Random rand;
     //constructor
     public Dice() {
-
+        this.rand = new Random();
     }
      
     //roll dice value
-    public rollDice() {
-        
+    public int rollDice() {
+        return rand.nextInt(6);
     }
 }
