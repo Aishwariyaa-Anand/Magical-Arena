795d660bab71301810e1f880e58e27155f97278b
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-07-10 10:25:56 +0530

    game structure

---
+++ magicalArena.java
@@ -0,0 +1,14 @@
+public class magicalArena {
+    private Player playerA;
+    private Player playerB;
+    
+    //constructor
+    public magicalArena() {
+
+    }
+
+    //fight
+    public Fight() {
+
+    }
+}
---
+++ player.java
@@ -0,0 +1,25 @@
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    //constructor
+    public Player() {
+
+    }
+
+    //attackplayer
+    public attackplayer() {
+
+    }
+
+    //defend
+    public defend() {
+
+    }
+
+    //tostring
+    public String toString() {
+        
+    }
+}
